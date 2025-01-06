389d55ae145aa528fdfe475d7c2e58503dcd661e
theskiingfox <theskiingfox@gmail.com>
2025-01-06 16:09:04 +0200

    am scos name field enervant

--- src/config/supabase.js
+++ src/config/supabase.js
@@ -3,9 +3,11 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://uglepmiodcpvafubwrvj.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVnbGVwbWlvZGNwdmFmdWJ3cnZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ1NTc0NjIsImV4cCI6MjA1MDEzMzQ2Mn0.HbOxsCFEPA6dV6QMl9QRv1YBXC1m6CBhBgkh_huU9eo'
 
-// Create Supabase client
+// Create Supabase client without auth redirect
 export const supabase = createClient(supabaseUrl, supabaseKey, {
     auth: {
-        redirectTo: 'https://vaisi.github.io/space-swoosh/'
+        persistSession: false,
+        autoRefreshToken: false,
+        detectSessionInUrl: false
     }
 }) 
