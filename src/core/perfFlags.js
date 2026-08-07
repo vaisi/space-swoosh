f05131554fc0d11c86654e9e1777d7e4877ba18c
theskiingfox <theskiingfox@gmail.com>
2026-08-07 14:19:42 +0300

    Revert "Run clock-true for ~70 ms after a touch during a run (?noinputraw=1 off)"
    This reverts commit 831164f1f4256d477a952f2ae376ee140e041b90.

--- src/core/perfFlags.js
+++ src/core/perfFlags.js
@@ -15,7 +15,6 @@
  * @property {number|null} dprOverride
  * @property {boolean} noHitchPass  ?nohitchpass=1: disable hitch passthrough (plain EMA, pre-experiment behavior)
  * @property {boolean} noGovernor   ?nogovernor=1: disable the menu 30 Hz idle governor
- * @property {boolean} noInputRaw   ?noinputraw=1: disable clock-true pacing in the post-touch window
  */
 
 /** @returns {PerfFlags} */
@@ -51,7 +50,6 @@ export function parsePerfFlags(search = typeof location !== 'undefined' ? locati
         dprOverride,
         noHitchPass: params.has('nohitchpass'),
         noGovernor: params.has('nogovernor'),
-        noInputRaw: params.has('noinputraw'),
     };
 }
 
